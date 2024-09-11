92382b0cdad37ecf0615039c449cf199cc8a98d8
adeebachurail <adeebaarif1322@gmail.com>
2024-09-11 09:05:07 +0500

    files added

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var questions = [
+    {
+        question: "what is 2 * 7 ?",
+        choice1: "14",
+        choice1: "15",
+        choice1: "16",
+        choice1: "17",
+        answer: 0
+    },
+{
+    question: "what is 9 / 3 ?",
+        choice1: "2",
+        choice1: "5",
+        choice1: "8",
+        choice1: "3",
+        answer: 3 
+}
+]
+
